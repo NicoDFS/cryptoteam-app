fdb8fd520b28c00811adf58675b8ef8c18cfb35d
youssefmaged95 <youssefmaged95@gmail.com>
2018-01-10 03:13:47 +0200

    Update player card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import PlayerCard from './Components/PlayerCard/'
-import { Container, Row } from 'react-grid-system';
+import PlayerCard from './Components/playerCard/PlayerCard'
+import { Row, } from 'antd';
 import './App.css';
 
 class App extends Component {
@@ -8,30 +8,20 @@ class App extends Component {
 
     return (
       <div className="App" style={divStyle}>
-        <Container>
-          <Row className="show-grid">
+          <Row  type="flex" justify="center" >
             <PlayerCard  
               name='Neymar Jr.' 
-              price={0.4} 
-              photo='https://www.footyrenders.com/render/Neymar-35-301x540.png'
             />            
             <PlayerCard  
               name='Lionel Messi' 
-              price={0.4} 
-              photo='https://www.footyrenders.com/render/Lionel-Messi-27-335x540.png'
             />
             <PlayerCard  
               name='M.Salah' 
-              price={0.4} 
-              photo='https://www.footyrenders.com/render/Mohamed-Salah-9-317x540.png'
             />
             <PlayerCard  
               name='Paul Pogba' 
-              price={0.4} 
-              photo='https://www.footyrenders.com/render/paul_pogba_by_igorband-dbjjbpo-386x540.png'
             />                                 
           </Row>
-        </Container>
       </div>
     );
   }
---
+++ src/Components/playerCard/playerCard.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import { Col } from 'antd';
+import './PlayerCard.css';
+
+let goldCard= require('../../Assets/cards/gold2.png');
+
+export default class PlayerCard extends Component {
+  
+  render() {
+
+    return (
+      <div className="container">
+
+          <div className="leftBar">
+            <p className="rating">93</p>
+            <p className="position">RW</p>
+            <img className="club" src="https://fifa17.content.easports.com/fifa/fltOnlineAssets/B1BA185F-AD7C-4128-8A64-746DE4EC5A82/2018/fut/items/images/clubbadges/html5/dark/24x24/l241.png" alt=""/>
+            <img className="nation" src="https://fifa17.content.easports.com/fifa/fltOnlineAssets/B1BA185F-AD7C-4128-8A64-746DE4EC5A82/2018/fut/items/images/flags/html5/24x14/52.png" alt=""/>
+          </div>
+
+          <div className="rightBar">
+            <img className='photo' src="https://fifa17.content.easports.com/fifa/fltOnlineAssets/B1BA185F-AD7C-4128-8A64-746DE4EC5A82/2018/fut/items/images/players/html5/134x134/158023.png" alt=""/>
+            <p className="name">{this.props.name}</p>
+            <div className="statsContainer">
+
+              <div className="rightStat">
+                <p className="statValue">89</p>
+                <p className="statName">PAC</p>
+              </div>
+
+              <div className="leftStat">
+                <p className="statValue">91</p>
+                <p className="statName">DRI</p>
+              </div>
+
+              <div className="rightStat">
+                <p className="statValue">78</p>
+                <p className="statName">SHO</p>
+              </div>
+
+              <div className="leftStat">
+                <p className="statValue">99</p>
+                <p className="statName">DEF</p>
+              </div>
+
+              <div className="rightStat">
+                <p className="statValue">73</p>
+                <p className="statName">PAS</p>
+              </div>
+
+              <div className="leftStat">
+                <p className="statValue">92</p>
+                <p className="statName">PHY</p>
+              </div>
+
+            </div>
+          </div>
+
+      </div>
+    )
+  }
+}
+
+
+
