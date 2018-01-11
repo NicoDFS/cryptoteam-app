20d7423a955784df05e53326af440855a8685a04
youssefmaged95 <youssefmaged95@gmail.com>
2018-01-11 08:17:55 +0200

    Rename folders

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import MarketPage from './Components/marketPage/MarketPage'
+import MarketPage from './components/marketPage/MarketPage'
 import './App.css';
 
 class App extends Component {
