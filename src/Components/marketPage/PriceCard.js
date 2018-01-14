f94964d1df71563747b748aa6d442244ecdfb5d0
youssefmaged95 <youssefmaged95@gmail.com>
2018-01-14 03:23:20 +0200

    Change price card style

--- src/Components/marketPage/PriceCard.js
+++ src/Components/marketPage/PriceCard.js
@@ -5,11 +5,9 @@ import './PriceCard.css';
 export default class componentName extends Component {
   render() {
     return (
-        <div>
       <div className="cardContainer">
         <PlayerCard playerInfo={this.props.playerInfo} />
-      </div>
-      <p className="price" >{this.props.playerInfo.price} ETH</p>
+        <p className="price" >{this.props.playerInfo.price} ETH</p>
       </div>
     )
   }
