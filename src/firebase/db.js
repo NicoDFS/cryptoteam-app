c7b9ab5016b658cb4c0bffaa2b70d8dee180436d
ninesalt <youssefswailem@gmail.com>
2018-01-15 15:17:50 +0200

    Add `placeForSale` function

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -2,9 +2,35 @@ import firebase from './index';
 let db = firebase.database();
 
 //player info is an object containing all relevant player data
-export default function addPlayer(playerInfo) {
+function addPlayer(playerInfo) {
     let pushRef = db.ref('/players').push();
     pushRef.set(playerInfo);
     let id = pushRef.key;
     return id;
 }
+
+function placeForSale(playerID, seller, price) {
+
+    //make sure the player exists in /players 
+    db.ref('/players/' + playerID).once('value').then((snapshot) => {
+
+        if (snapshot.val() === null) {
+            return undefined;
+        }
+
+        else {
+            let pushRef = db.ref('/market').push();
+            pushRef.set({ player: snapshot.val(), seller: seller, price: price });
+            let id = pushRef.key;
+            return id;
+        }
+
+    });
+
+}
+
+function getMarket() {
+
+}
+
+export { addPlayer, placeForSale, getMarket }
