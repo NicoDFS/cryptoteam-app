6da9a65b77116dae013f64044b15e2f88be9c049
ninesalt <youssefswailem@gmail.com>
2018-01-17 20:04:57 +0200

    Increase filter width

--- src/components/MarketPage/Filter/Filter.js
+++ src/components/MarketPage/Filter/Filter.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react'
-import {Row} from 'antd'
+import { Row } from 'antd'
 import './Filter.css'
 import Dropdown from './Dropdown/Dropdown'
+
 export default class Filter extends Component {
 
-  getSort( field ){
-    console.log(field);
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      sortBy: 'rating'
+    }
   }
+
 
   render() {
     return (
+
       <Row className="filterContainer" type="flex" justify="left">
-        <input placeholder="Search" className="input"/>
-        <Dropdown items={['Price','Rating']} title="Sort by: " onChange={this.getSort}/>
-        <Dropdown items={['Lowest first','Highest first']} onChange={this.getSort}/>
+        <input placeholder="Search" className="input" />
+
+        <Dropdown items={['Rating', 'Price ascending', 'Popularity', 'Price descending']}
+          title="Sort by: " onChange={this.props.sortBy} />
+
       </Row>
     )
   }
