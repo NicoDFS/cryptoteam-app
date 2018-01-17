03e64a4a4619c4b4e0822411b745988bdfda7043
ninesalt <youssefswailem@gmail.com>
2018-01-17 19:37:25 +0200

    Update imports

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
 import Web3Test from './test/testweb3';
-import MarketPage from './Components/MarketPage/MarketPage'
-import NavBar from './Components/NavBar/NavBar'
-import Web3Test from './Components/web3/testweb3';
+import MarketPage from './components/MarketPage/MarketPage'
+import NavBar from './components/NavBar/NavBar'
 
-// import './App.css'
-import 'antd/dist/antd.css';
+import './App.css'
+// import 'antd/dist/antd.css';
 
 class App extends Component {
 
   render() {
 
     return (
-      // <div className="App" style={divStyle}>
-      //   <NavBar title="cryptoteam" />
-      //   <MarketPage />
-      // </div>
-      <Web3Test web3={this.props.web3} />
+      <div className="App" style={divStyle}>
+        <NavBar title="cryptoteam" />
+        <MarketPage />
+      </div>
+      // <Web3Test web3={this.props.web3} />
     );
   }
 }
