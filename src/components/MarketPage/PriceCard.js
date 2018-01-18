56c0c8becb973c5fbfa87be4d87a026e35ebed46
ninesalt <youssefswailem@gmail.com>
2018-01-19 00:05:00 +0200

    Update price card and add hover animation

--- src/components/MarketPage/PriceCard.js
+++ src/components/MarketPage/PriceCard.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
+import { Card } from 'antd';
 import PlayerCard from '../PlayerCard/PlayerCard'
 import './PriceCard.css';
+
 
 export default class PriceCard extends Component {
   render() {
     return (
-      <div className="cardContainer">
-        <PlayerCard playerInfo={this.props.playerInfo} />
+      <Card className="cardContainer-ant">
+        <PlayerCard playerInfo={this.props.playerInfo} /> <br />
         <p className="price" >{this.props.playerInfo.price} ETH</p>
-      </div>
+      </Card>
     )
   }
 }
