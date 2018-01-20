38b5393e96cab998364de4d8f3cc56f719c2068b
ninesalt <youssefswailem@gmail.com>
2018-01-20 22:32:57 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react';
-import Web3Test from './test/testweb3';
-import MarketPage from './components/MarketPage/MarketPage'
-import NavBar from './components/NavBar/NavBar'
-import PlayerPage from './components/PlayerPage/PlayerPage'
-
+import RouterConfig from './router';
 import './App.css'
-// import 'antd/dist/antd.css';
+
 
 class App extends Component {
 
   render() {
 
     return (
-      <div className="App">
-        <NavBar title="Cryptoteam" />
-        <MarketPage />
-      </div>
-      // <Web3Test web3={this.props.web3} />
+      <RouterConfig web3={this.props.web3} />
     );
   }
 }
---
+++ src/router.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { HashRouter, Route } from 'react-router-dom'
+
+// Pages
+import MarketPage from './components/MarketPage/MarketPage'
+import NavBar from './components/NavBar/NavBar'
+import PlayerPage from './components/PlayerPage/PlayerPage'
+import AuthPage from './components/AuthPage/AuthPage'
+import HomePage from './components/HomePage/HomePage'
+
+
+class RouterConfig extends Component {
+
+    render() {
+
+        return (
+
+            <HashRouter>
+                <div className="App">
+
+                    {/* Static section */}
+                    <div>
+                        <NavBar title="Cryptoteam" />
+                    </div>
+
+                    {/* Dynamic section */}
+                    <div>
+
+                        <Route path="//" component={HomePage} />
+
+                        <Route path="/market"
+                            render={() => <MarketPage web3={this.props.web3} />} />
+
+                        <Route path="/player" component={PlayerPage} />
+
+                    </div>
+
+                </div>
+            </HashRouter>
+
+        );
+    }
+}
+
+
+
+export default RouterConfig;
