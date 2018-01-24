2d0857b6dddd11acd473f4419f3b158a6c96d918
ninesalt <youssefswailem@gmail.com>
2018-01-24 17:22:08 +0200

    Add conditional rendering for header

--- src/components/CustomContent/CustomContent.js
+++ src/components/CustomContent/CustomContent.js
@@ -4,29 +4,33 @@ next to sidebar.
 */
 
 import React, { Component } from 'react';
-import { Layout } from 'antd';
+import { Layout, Col } from 'antd';
 import './CustomContent.css';
 const { Header, Content } = Layout;
 
 export default class CustomContent extends Component {
 
-    constructor(props) {
-        super(props)
-    }
-
     render() {
-        return (
-            <Layout>
 
-                <Header className="custom-content-navbar">
-                    <p className="custom-content-logo">{this.props.title}</p>
-                </Header>
+        let header = null;
+
+        //only render a title if one was passed as a prop
+        if (this.props.title) {
+            header = <Header className="custom-content-navbar">
+                <p className="custom-content-logo">{this.props.title}</p>
+            </Header>
+        }
+
+        return (
+            <Layout className="custom-layout">
+
+                {header}
 
                 <Content className="custom-content">
                     {this.props.content}
                 </Content>
 
-            </Layout>
+            </Layout >
         )
     }
 }
