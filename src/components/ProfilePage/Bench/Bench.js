f08e4240446a7a320a7cf32cd4d6a000a2bdba42
ninesalt <youssefswailem@gmail.com>
2018-02-10 17:04:05 +0200

    Add MM unavailable screen

--- src/components/ProfilePage/Bench/Bench.js
+++ src/components/ProfilePage/Bench/Bench.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 import BenchItem from './BenchItem';
 import { Row } from 'antd';
+import { Web3Provider } from 'react-web3';
+import Web3Unavailable from '../../Web3/Unavailable';
+
 
 export default class Bench extends Component {
     render() {
         return (
-            <div>
-                {/* <p className="benchCount">12 Players</p> */}
-                <Row type="flex" justify="center" >
-                    {this.props.players.map((item, index) => (
-                        <BenchItem web3={this.props.web3}
-                            key={index}
-                            player={item} />
-                    ))}
-                </Row>
-            </div>
+
+            <Web3Provider
+                web3UnavailableScreen={Web3Unavailable}
+                accountUnavailableScreen={Web3Unavailable}>
+
+                <div>
+                    {/* <p className="benchCount">12 Players</p> */}
+                    <Row type="flex" justify="center" >
+                        {this.props.players.map((item, index) => (
+                            <BenchItem web3={this.props.web3}
+                                key={index}
+                                player={item} />
+                        ))}
+                    </Row>
+                </div>
+
+            </Web3Provider>
         )
     }
 }
