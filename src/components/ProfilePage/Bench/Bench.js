56558a1360b920eacd8e269ab6cad36ac0ac39a2
tirex22 <youssefmaged95@gmail.com>
2018-03-01 16:05:46 +0200

    Add Pagination to bench page

--- src/components/ProfilePage/Bench/Bench.js
+++ src/components/ProfilePage/Bench/Bench.js
@@ -3,84 +3,23 @@ import BenchItem from './BenchItem';
 import { Row } from 'antd';
 import { Web3Provider } from 'react-web3';
 import Web3Unavailable from '../../Web3/Unavailable';
-import Filter from '../../Generic/Filter/Filter'
-import { chunk } from 'lodash';
-let accent_clean = require('remove-accents');
 
-let filters = ['Rating', 'Popularity'];
 
 export default class Bench extends Component {
 
     constructor() {
         super();
         this.state = {
-            bench_static: [],
             bench: [],
         }
     }
 
     componentDidMount() {
-        this.setState({ bench_static: this.props.players, bench: this.props.players });
+        this.setState({ bench: this.props.players });
     }
 
     componentWillReceiveProps(nextProps) {
-        this.setState({ bench_static: this.props.players, bench: nextProps.players });
-    }
-
-    onSort(index) {
-        let temp = this.state.bench_static;
-        temp.sort((a, b) => {
-            switch (index) {
-                case 1:
-                    return a.info.popularity - b.info.popularity; //popularity descending (not ready yet)
-                default:
-                    return b.info.rating - a.info.rating; //sort by rating (case 0)
-            }
-        });
-
-        this.setState({ bench: temp });
-
-        // if (temp.length > 0) {
-        //     let market_split = chunk(temp, this.state.cards_per_page);
-        //     this.setState({
-        //         market: market_split[0],
-        //         market_split: market_split
-        //     });
-        // }
-    }
-
-    onSearch(term) {
-        let temp = this.state.bench_static;
-
-        temp = temp.filter((a) => {
-            return accent_clean(a.info.name.toLowerCase())
-                .indexOf(term.toLowerCase()) !== -1 ||
-                a.info.name.toLowerCase()
-                    .indexOf(term.toLowerCase()) !== -1
-        });
-
-        this.setState({ bench: temp });
-
-        // if (temp.length > 0) {
-        //     let market_split = chunk(temp, this.state.cards_per_page);
-        //     this.setState({
-        //         market: market_split[0],
-        //         market_split: market_split,
-        //         no_results: false
-        //     });
-        // }
-        // // no search results
-        // else {
-        //     this.setState({ market: [], no_results: true });
-        // }
-        // window.scrollTo(0, 85);
-    }
-
-    onSearchChange(searchTerm) {
-        if (searchTerm === '' || searchTerm === ' ') {
-            // let market = this.state.market_split[this.state.current_page];
-            this.setState({ bench: this.state.bench_static, no_results: false });
-        }
+        this.setState({ bench: nextProps.players });
     }
 
     render() {
@@ -91,10 +30,6 @@ export default class Bench extends Component {
                 accountUnavailableScreen={Web3Unavailable}>
 
                 <div>
-                    <Filter onSort={(e) => this.onSort(e)}
-                        onSearch={(e) => this.onSearch(e)}
-                        onSearchChange={(e) => this.onSearchChange(e)}
-                        filters={filters} />
 
                     {/* <p className="benchCount">12 Players</p> */}
                     <Row type="flex" justify="center" >
@@ -104,6 +39,9 @@ export default class Bench extends Component {
                                 player={item} />
                         ))}
                     </Row>
+
+
+
                 </div>
 
             </Web3Provider>
--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -5,7 +5,13 @@ import { getUser } from '../../firebase/db'
 import './ProfilePage.css'
 import authenticate from '../../firebase/auth'
 import NoResults from './NoResults'
-import { Row } from 'antd'
+import { Row, Pagination } from 'antd'
+import { chunk } from 'lodash';
+import Filter from '../Generic/Filter/Filter'
+
+
+let accent_clean = require('remove-accents');
+let filters = ['Rating', 'Popularity'];
 let web3;
 
 export default class ProfilePage extends Component {
@@ -14,6 +20,11 @@ export default class ProfilePage extends Component {
     super(props);
     this.state = {
       bench: [],
+      bench_static: [],
+      bench_split: [],
+      cards_per_page: 10,
+      current_page: 0,
+      loaded: true,
       userAddress: '',
       no_results: false,
     }
@@ -48,7 +59,15 @@ export default class ProfilePage extends Component {
 
           // setting bench state after getting all players data
           if (index === playerIds.length - 1) {
-            this.setState({ bench: bench });
+
+            let bench_split = chunk(bench, this.state.cards_per_page);
+
+            this.setState({
+              bench: bench_split[0],
+              bench_static: bench,
+              bench_split: bench_split,
+              loaded: true,
+            });
           }
         });
         this.setState({ userAddress: address });
@@ -57,6 +76,81 @@ export default class ProfilePage extends Component {
       }
     });
 
+  }
+
+  updatePagination(pageNumber) {
+    let cards = this.state.bench_split[pageNumber - 1];
+    window.scrollTo(0, 85);
+    this.setState({ bench: cards, current_page: pageNumber - 1 });
+  }
+
+  onShowSizeChange(number, size) {
+
+    let bench = this.state.bench_static;
+    let split = chunk(bench, size);
+
+    this.setState({
+      cards_per_page: size,
+      bench_split: split,
+      bench: split[number - 1]
+    });
+    window.scrollTo(0, 85);
+  }
+
+  onSort(index) {
+    let temp = this.state.bench_static;
+    temp.sort((a, b) => {
+      switch (index) {
+        case 1:
+          return a.info.popularity - b.info.popularity; //popularity descending (not ready yet)
+        default:
+          return b.info.rating - a.info.rating; //sort by rating (case 0)
+      }
+    });
+
+    this.setState({ bench: temp });
+
+    if (temp.length > 0) {
+      let bench_split = chunk(temp, this.state.cards_per_page);
+      this.setState({
+        bench: bench_split[0],
+        bench_split: bench_split
+      });
+    }
+  }
+
+  onSearch(term) {
+    let temp = this.state.bench_static;
+
+    temp = temp.filter((a) => {
+      return accent_clean(a.info.name.toLowerCase())
+        .indexOf(term.toLowerCase()) !== -1 ||
+        a.info.name.toLowerCase()
+          .indexOf(term.toLowerCase()) !== -1
+    });
+
+    this.setState({ bench: temp });
+
+    if (temp.length > 0) {
+      let bench_split = chunk(temp, this.state.cards_per_page);
+      this.setState({
+        bench: bench_split[0],
+        bench_split: bench_split,
+        no_results: false
+      });
+    }
+    // no search results
+    else {
+      this.setState({ bench: [], no_results: true });
+    }
+    window.scrollTo(0, 85);
+  }
+
+  onSearchChange(searchTerm) {
+    if (searchTerm === '' || searchTerm === ' ') {
+      let bench = this.state.bench_split[this.state.current_page];
+      this.setState({ bench: bench, no_results: false });
+    }
   }
 
   render() {
@@ -70,11 +164,35 @@ export default class ProfilePage extends Component {
     return (
       <CustomContent title="Bench"
         content={
-          <Row type="flex"
-            justify="center">
-            <Bench web3={this.props.web3} players={this.state.bench} />
-            {no_results}
-          </Row>
+
+          <div>
+
+            <Filter onSort={(e) => this.onSort(e)}
+              onSearch={(e) => this.onSearch(e)}
+              onSearchChange={(e) => this.onSearchChange(e)}
+              filters={filters} />
+
+            <Row type="flex"
+              justify="center">
+
+              <Bench web3={this.props.web3} players={this.state.bench} />
+
+              {no_results}
+
+            </Row>
+
+            <Pagination showSizeChanger defaultCurrent={1}
+              style={{
+                marginBottom: 30,
+                marginTop: 30,
+                display: this.state.loaded && !this.state.no_results ? 'block' : 'none'
+              }}
+              onShowSizeChange={(current, size) => this.onShowSizeChange(current, size)}
+              total={this.state.bench_split.length * 10}
+              pageSizeOptions={['15', '30', '40']}
+              onChange={(number, size) => this.updatePagination(number, size)} />
+
+          </div>
         } />
     )
   }
